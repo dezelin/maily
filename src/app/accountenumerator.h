176fe3b09386c4b3190c3c2b65aae7661646b871
Aleksandar Dezelin <dezelin@gmail.com>
2011-11-17 03:06:41 +0100

    Added stubs for a few account service classes.

---
+++ src/app/accountenumerator.h
@@ -0,0 +1,35 @@
+/*
+ *   This file is part of Maily.
+ *
+ *   Maily is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Maily is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Maily. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ACCOUNTENUMERATOR_H
+#define ACCOUNTENUMERATOR_H
+
+namespace Maily
+{
+namespace Services
+{
+
+class AccountEnumerator
+{
+public:
+    AccountEnumerator();
+};
+
+} // namespace Services
+} // namespace Maily
+
+#endif // ACCOUNTENUMERATOR_H
---
+++ src/app/accountmanager.h
@@ -0,0 +1,43 @@
+/*
+ *   This file is part of Maily.
+ *
+ *   Maily is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Maily is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Maily. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ACCOUNTMANAGER_H
+#define ACCOUNTMANAGER_H
+
+#include <QObject>
+
+namespace Maily
+{
+namespace Services
+{
+
+class AccountManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AccountManager(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+} // namespace Services
+} // namespace Maily
+
+#endif // ACCOUNTMANAGER_H
