ea848229d8feff6888c3f5e52eec64e0c0dfa31e
Aleksandar Dezelin <dezelin@gmail.com>
2011-11-19 02:44:30 +0100

    Code reformatted.

--- src/app/busyindicator.cpp
+++ src/app/busyindicator.cpp
@@ -27,15 +27,10 @@ namespace Maily
 namespace Widgets
 {
 
-BusyIndicator::BusyIndicator(QDeclarativeItem* parent)
-    : QDeclarativeItem(parent),
-      m_innerRadius(0.8),
-      m_outerRadius(1.0),
-      m_backgroundColor(177, 210, 143, 70),
-      m_foregroundColor(119, 183, 83, 255),
-      m_actualInnerRadius(0.0),
-      m_actualOuterRadius(0.0),
-      m_cacheKey()
+BusyIndicator::BusyIndicator(QDeclarativeItem* parent) :
+    QDeclarativeItem(parent), m_innerRadius(0.8), m_outerRadius(1.0),
+    m_backgroundColor(177, 210, 143, 70), m_foregroundColor(119, 183, 83, 255),
+    m_actualInnerRadius(0.0), m_actualOuterRadius(0.0), m_cacheKey()
 {
     setFlag(QGraphicsItem::ItemHasNoContents, false);
     setWidth(16.0);
