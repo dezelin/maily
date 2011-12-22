153675b3ce1822278d434d364e6b78f268152da9
Aleksandar Dezelin <dezelin@gmail.com>
2011-12-22 20:09:50 +0100

    Prepared dependencies for compilation under Mingw32.

--- src/thirdparty/prepare_mingw32.sh
+++ src/thirdparty/prepare_mingw32.sh
@@ -16,6 +16,42 @@ cd libgsasl-1.6.1
 configure && make && make install && make clean && make distclean
 cd ..
 
+cd zlib-1.2.5
+make -f win32/Makefile.gcc
+cp zlib.h /usr/local/include/
+cp zconf.h /usr/local/include/
+cp libz.a /usr/local/lib/
+cp libz.dll.a /usr/local/lib/
+make -f win32/Makefile.gcc clean
+cd ..
+
+cd libgpg-error-1.10
+configure && make
+rm po/*.po
+make install && make clean && make distclean
+cd ..
+
+cd libgcrypt-1.5.0
+configure && make && make install & make clean && make distclean
+cd ..
+
+cd gnutls-2.12.9
+configure --disable-guile --enable-threads=win32 --with-libgcrypt --without-p11-kit
+touch lib/libgnutls-26.def
+touch lib/libgnutls-extra-26.def
+touch lib/libgnutls-openssl-26.def
+make 
+#
+# the first call to make will stop with compilation errors so restart it
+#
+make && make install && make clean && make distclean
+cd ..
+
 cd libvmime-0.9.1
-configure && make && make install && make clean && make distclean
+export CPPFLAGS="-I/usr/local/include/"
+export LDFLAGS="-L/usr/local/lib/"
+export LIBGNUTLS_CFLAGS="-I/usr/local/include/"
+export LIBGNUTLS_LIBS="-L/usr/local/lib/ -lgnutls -lgcrypt -L/usr/local/lib -lgpg-error -lws2_32 -R/usr/local/lib -L/usr/local/lib -lz -R/usr/local/lib"
+configure --enable-debug --enable-shared
+make && make install && make clean
 cd ..
